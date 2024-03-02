24937933ed6eea60490ffa5359a8e9ef6ed05bd5
v.adhi <vr1090@gmail.com>
2024-03-02 10:57:41 +0700

    edit convert.sh

--- convert.sh
+++ convert.sh
@@ -2,5 +2,5 @@
 # convert anamafolder
 # end up jadi namafolder.mbox
 
-find $1 -name "*.msg" | xargs -I {} python3 ./msg2eml.py {}
+find $1 -name "*.msg" | xargs -I {} python3 ./src/msgtombox/msg2eml.py {}
 python3 ./emltombox.py $1 $1.mbox
